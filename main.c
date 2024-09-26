606280c55243719cb7d718e28f3419bf594adbc3
Anselme <anselmedupuy@gmail.com>
2024-09-26 12:26:10 +0200

    Ajout If

--- main.c
+++ main.c
@@ -1,12 +1,31 @@
+#include <stdlib.h>
 #include <stdio.h>
-#include<stdio.h>
 
 int main()
  {
- 	int nombre1, nombre2, operateur;
+ 	int nombre1, nombre2;
+ 	char operateur;
 
- 	printf("Choisir un symbole:\n + - / * %");
- 	scanf("%d", &operateur);
+ 	printf("Choisir un symbole:\n +\n -\n /\n *\n %%\n");
+ 	scanf("%c", &operateur);
+
+ 	if (operateur == '+')
+ 	{
+ 		printf("addition\n");
+ 	}
+ 	else if(operateur == '-')
+ 	{
+ 		printf("soustraction\n");
+ 	}
+ 	else if(operateur == '/')
+ 	{
+ 		printf("division\n");
+ 	}
+ 	else if(operateur == '*')
+ 	{
+ 		printf("multiplication\n");
+ 	}
+ 	
 
  	exit(0);
  } 
