dc07d8643e1f6fca4385a0d889cf96934b4c9f00
Anselme <anselmedupuy@gmail.com>
2024-09-26 14:00:47 +0200

    Commit final

--- main.c
+++ main.c
@@ -9,21 +9,51 @@ int main()
  	printf("Choisir un symbole:\n +\n -\n /\n *\n %%\n");
  	scanf("%c", &operateur);
 
+ 	printf("Choisir le premier nombre: ");
+ 	scanf("%d", &nombre1);
+
+ 	printf("Choisir le second nombre: ");
+ 	scanf("%d", &nombre2);
+
  	if (operateur == '+')
  	{
- 		printf("addition\n");
+ 		int resultat = nombre1 + nombre2;
+ 		printf("addition, %d + %d = %d\n", nombre1, nombre2, resultat);
+
  	}
  	else if(operateur == '-')
  	{
- 		printf("soustraction\n");
+ 		int resultat = nombre1 - nombre2;
+ 		printf("soustraction, %d - %d = %d\n", nombre1,nombre2,resultat);
  	}
  	else if(operateur == '/')
  	{
- 		printf("division\n");
+ 		if ((nombre1 == 0)||(nombre2 == 0))
+ 		{
+ 			printf("division par 0 impossible");
+ 		}
+ 		else
+ 		{
+ 			int resultat = nombre1 / nombre2;
+ 			printf("division, %d / %d = %d\n",nombre1, nombre2, resultat);
+ 		}
  	}
  	else if(operateur == '*')
  	{
- 		printf("multiplication\n");
+ 		int resultat = nombre1 * nombre2;
+ 		printf("multiplication, %d * %d = %d\n",nombre1, nombre2,resultat);
+ 	}
+ 	else if(operateur == '%')
+ 	{
+ 		if ((nombre1 == 0)||(nombre2 ==0))
+ 		{
+ 			printf("division par 0 impossible\n");
+ 		}
+ 		else
+ 		{
+ 			int resultat = nombre1 % nombre2;
+ 			printf("modulo, %d %% %d = %d\n",nombre1, nombre2, resultat);
+ 		}
  	}
  	
 
